f619e050c0644c60ada997394de1ffbfccbd77fa
revido <revidoberliano@gmail.com>
2022-07-18 15:15:18 +0700

    tambah qrcode

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -10,8 +10,11 @@
 
     <!-- General CSS Files -->
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- Template CSS -->
@@ -150,6 +153,11 @@
             </div>
             <div class="modal-body" id="bukti">
 
+
+                <a rel='nofollow' href='https://www.qr-code-generator.com' border='0' style='cursor:default'>
+                    <img style="display: block; margin-left: auto; margin-right: auto;"
+                        src='https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fshare.fectar.com%2FTYRXK&chs=180x180&choe=UTF-8&chld=L|2'
+                        alt=''></a>
             </div>
         </div>
     </div>
